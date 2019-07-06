709a41fb97adb1c57bdd5f3e75603a09704d0543
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-06 14:00:07 -0500

    Created new routes and component for Recipes(new, mine)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   #app
     .bg-purple-500.p-4.text-white
       router-link(to="/" class="hover:text-gray-300 font-bold").m-2 Recipes
-      router-link(to="/about" class="hover:text-gray-300").m-2 About
+      router-link(to="/recipes/new" class="hover:text-gray-300").m-2 New Recipe
+      router-link(to="/recipes/mine" class="hover:text-gray-300").m-2 My Recipes
     
     router-view
 </template>
