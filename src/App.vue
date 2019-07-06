513aa37eaaa439259f201b1ae07d96755a03efde
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-06 13:55:44 -0500

    Update templates to pug

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<template>
-  <div id="app">
-    <div class="bg-blue-100">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app
+    .bg-purple-500.p-4.text-white
+      router-link(to="/" class="hover:text-gray-300 font-bold").m-2 Recipes
+      router-link(to="/about" class="hover:text-gray-300").m-2 About
+    
+    router-view
 </template>
