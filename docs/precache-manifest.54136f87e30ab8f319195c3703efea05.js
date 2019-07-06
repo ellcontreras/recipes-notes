4d608eef89fad0646f48c8d6002917390053a5d7
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-06 16:51:50 -0500

    Folder to production

---
+++ docs/precache-manifest.54136f87e30ab8f319195c3703efea05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6016a0560d9b8f07750",
+    "url": "/css/app.b7e129fd.css"
+  },
+  {
+    "revision": "b6016a0560d9b8f07750",
+    "url": "/js/app.059a8162.js"
+  },
+  {
+    "revision": "806c77ef4bda74d4333c",
+    "url": "/js/chunk-vendors.864a3a59.js"
+  },
+  {
+    "revision": "149084e9d86298507606c3049560b184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
